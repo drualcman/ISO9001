3ca2d1e5661691ca3eba8ef49fb013c6a1640118
Sergi Ortiz Gomez <drualcman@gmail.com>
2025-07-23 08:21:02 +0800

    Cambio en el interfaz para hacerlo generico, replicar en todos los interfaces de contexto

--- Src/ISO9001.AuditLogs.Repositories/Interfaces/IQueryableAuditLogDataContext.cs
+++ Src/ISO9001.AuditLogs.Repositories/Interfaces/IQueryableAuditLogDataContext.cs
@@ -5,8 +5,8 @@ namespace ISO9001.AuditLogs.Repositories.Interfaces
     public interface IQueryableAuditLogDataContext
     {
         IQueryable<AuditLogReadModel> AuditLogs { get; }
-        Task<IEnumerable<ReturnType>> ToListAsync<ReturnType>(
-            IQueryable<ReturnType> queryable);
+        Task<IEnumerable<AuditLogReadModel>> ToListAsync(
+            IQueryable<AuditLogReadModel> queryable);
     }
 }
 
