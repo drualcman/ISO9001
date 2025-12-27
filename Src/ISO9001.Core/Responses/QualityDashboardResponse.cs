262c539bf1832f39af5b5c96e134f3255436a23d
EduardoRMgit <122333594+EduardoRMgit@users.noreply.github.com>
2025-12-26 22:54:32 -0600

    chore: remove comments

--- Src/ISO9001.Core/Responses/QualityDashboardResponse.cs
+++ Src/ISO9001.Core/Responses/QualityDashboardResponse.cs
@@ -5,11 +5,11 @@ public class QualityDashboardResponse(int openNonConformities, int closedNonConf
     Dictionary<string, int> incidentsPerOrder, int totalIncidentReports,
     List<MonthlyQualityKpi> monthlyKpis)
 {
-    public int OpenNonConformities => openNonConformities; // YA
-    public int ClosedNonConformities => closedNonConformities; // YA
-    public TimeSpan AvarageResolutionDays => avarageResolutionDays; // YA
-    public int TotalFeedbacks => totalFeedbacks; // YA
-    public double AvarageRating => avarageRating; // YA 
+    public int OpenNonConformities => openNonConformities;
+    public int ClosedNonConformities => closedNonConformities;
+    public TimeSpan AvarageResolutionDays => avarageResolutionDays;
+    public int TotalFeedbacks => totalFeedbacks;
+    public double AvarageRating => avarageRating;
     public Dictionary<string, int> IncidentsPerOrder => incidentsPerOrder;
     public int TotalIncidentReports => totalIncidentReports;
     public List<MonthlyQualityKpi> MonthlyKpis => monthlyKpis;
